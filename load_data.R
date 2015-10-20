3a468c8c4f897a6048b1653a71fe6eb24152a98e
François ROLAND <francois.roland@hermes-ecs.com>
2015-10-21 00:17:57 +0200

    Initial exploratory analysis.

--- load_data.R
+++ load_data.R
@@ -1,18 +1,18 @@
+library(data.table)
+library(R.utils)
+
 read_StormData <- function() {
-    file_path <- "data/StormData.csv.bz2"
-    if (!file.exists(file_path)) {
-        download.file(
-            "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2",
-            destfile = file_path)
-    }
-    read.csv("data/StormData.csv.bz2")
+    compressed_file_path <- "data/StormData.csv.bz2"
+    download.file(
+        "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2",
+        destfile = compressed_file_path)
+    uncompressed_file_path <- bunzip2(compressed_file_path)
+    data <- fread(uncompressed_file_path)
 }
 
 download_docs <- function() {
     download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fpeer2_doc%2Fpd01016005curr.pdf",
-                  destfile = "data/StormData_documentation.pdf")
+                  destfile = "data/StormData_documentation.pdf", mode = "wb")
     download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fpeer2_doc%2FNCDC%20Storm%20Events-FAQ%20Page.pdf",
-                  destfile = "data/FAQ.pdf")
+                  destfile = "data/FAQ.pdf", mode = "wb")
 }
-
-StormData <- read_StormData()
